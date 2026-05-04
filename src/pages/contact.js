c3d2fb5e3082a0bcee492b59a72a1daeff526697
Human-Centered Interactive Technologies Lab <90462458+hcitechlab@users.noreply.github.com>
2026-05-04 10:40:13 +0900

    260504

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -34,7 +34,7 @@ export default function Contact() {
           <h4>For M.S./Ph.D. students, please check below.</h4>
           <p></p>
           <Link className="badge bg-primary" href="/recruiting_graduate" style={{width: "15.5rem", height: "2.2rem", fontSize: "1.2rem",marginRight: "1rem" }}>M.S./Ph.D. Students</Link>
-          {/* <Link className="badge bg-primary" href="/recruiting_under" style={{width: "15.5rem", height: "2.2rem", fontSize: "1.2rem" }}>2026 Winter Internship</Link>*/}
+          <Link className="badge bg-primary" href="/internship/summer-2026" style={{width: "15.5rem", height: "2.2rem", fontSize: "1.2rem" }}>2026 Summer Internship</Link>
         </SectionContainer>
       </div>
     );
